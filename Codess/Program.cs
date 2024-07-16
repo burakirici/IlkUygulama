0c76f61b8ee6055dae7df3e0dfa45ff9363d3e30
Burak Kırıcı <burakpinar76@gmail.com>
2024-07-16 13:35:54 +0300

    AdSoyadVeSebep
    Ilk uygulamam

---
+++ Codess/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IlkUygulamam
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Burak Kirici\nPatika'nin uzman kadrosu ve tecrubeli egitmenleri ile yazilim sektorune hizli bir giris yapip basarili olacagima inandigim icin bu bootcampteyim!");
+        }
+    }
+}
